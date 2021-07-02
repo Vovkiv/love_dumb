fba4dacf6d1ad1e3541b2055cc37ed72dd751980
Vovkiv <54743395+Vovkiv@users.noreply.github.com>
2021-07-02 13:17:13 +0300

    11.3

--- fov/fov.lua
+++ fov/fov.lua
@@ -8,15 +8,21 @@ local module = {}
 module.abs = math.abs
 module.floor = math.floor
 
-module.march_line = function (map, view, c1, r1, c2, r2) -- source and target
+module.march_line = function (map, view, c1, r1, c2, r2, radius) -- source and target
 	local length = module.abs(c2 - c1) + module.abs(r2 - r1)
 	if length > 1 then
 		local dx = (c2 - c1) / length
 		local dy = (r2 - r1) / length
 		for n = 1, length do
-			local c, r = module.floor (c1 + n * dx + 0.5), module.floor (r1 + n * dy + 0.5)
+			local c, r = module.floor (c1 + n * dx + 0.5 + 0.00001 * dy), module.floor (r1 + n * dy + 0.5 - 0.00001 * dx)
+
+      if radius^2 <= (c1 - c)^2 + (r1 - r)^2 then
+				-- out of range for circle
+				return
+			end
+
 			view[c] = view[c] or {}
-			if map[c] and map[c][r] and map[c][r] == 0 then
+			if map[c] and map[c][r] and map[c][r] <= 0 then
 				-- not visible
 				view[c][r] = false
 				return
@@ -26,7 +32,7 @@ module.march_line = function (map, view, c1, r1, c2, r2) -- source and target
 			end
 		end
 	else
-		if map[c2] and map[c2][r2] and map[c2][r2] == 0 then
+		if map[c2] and map[c2][r2] and map[c2][r2] <= 0 then
 			view[c2] = view[c2] or {}
 			view[c2][r2] = false
 		end
@@ -36,12 +42,12 @@ end
 module.get = function (map, c, r, radius)
 	local view = {}
 	for c2 = c - radius, c + radius do
-		module.march_line (map, view, c, r, c2, r + radius)
-		module.march_line (map, view, c, r, c2, r - radius)
+		module.march_line (map, view, c, r, c2, r + radius, radius)
+		module.march_line (map, view, c, r, c2, r - radius, radius)
 	end
 	for r2 = r - radius + 1, c + radius - 1 do
-		module.march_line (map, view, c, r, c + radius, r2)
-		module.march_line (map, view, c, r, c - radius, r2)
+		module.march_line (map, view, c, r, c + radius, r2, radius)
+		module.march_line (map, view, c, r, c - radius, r2, radius)
 	end
 	return view
 end

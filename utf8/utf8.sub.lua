096aae8095467d2ed8ff694597a75db6fbc5022a
Vovkiv <54743395+Vovkiv@users.noreply.github.com>
2021-07-03 15:37:52 +0300

    Create utf8.sub.lua

---
+++ utf8/utf8.sub.lua
@@ -0,0 +1,13 @@
+local utf8 = require("utf8")
+
+function utf8.sub(str, x, y)
+  local x2, y2
+  x2 = utf8.offset(str, x)
+  if y then
+    y2 = utf8.offset(str, y+1)
+    if y2 then
+      y2 = y2 - 1
+    end
+  end
+  return string.sub(str, x2, y2)
+end

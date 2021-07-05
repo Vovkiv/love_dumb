5e6acb7f4e542a002d064ee1df55127519e3735c
Vovkiv <54743395+Vovkiv@users.noreply.github.com>
2021-07-05 21:34:23 +0300

    Update aspect.lua

--- aspect/aspect.lua
+++ aspect/aspect.lua
@@ -1,53 +1,41 @@
 -- https://github.com/Vovkiv/love_dumb/blob/main/aspect/aspect.lua
 
--- default
-local background = false -- if false, black bars will copy colors from love.graphics.getBackgroundColor, true - copy colors from r, g, b, a
+local copyBackgroundColor = false
 local gameWidth, gameHeight = 800, 600
 local r, g, b, a = 0, 0, 0, 1
 
--- love
 local lovePop, lovePush, loveTranslate, loveScale, loveColor, loveRectangle, loveBackground, loveWidth, loveHeight, abs = love.graphics.pop, love.graphics.push, love.graphics.translate, love.graphics.scale, love.graphics.setColor, love.graphics.rectangle, love.graphics.getBackgroundColor, love.graphics.getWidth, love.graphics.getHeight, math.abs
 
--- local
-local x1, y1, w1, h1, x2, y2, w2, h2 -- black bars positions
-local windowWidth, windowHeight      -- actual window size
-local xoff, yoff                     -- offset of black bars; if black bar width = 10, then xoff = 10, which can be used to correct mouse position
-local gameAspect, windowAspect 
+local x1, y1, w1, h1, x2, y2, w2, h2
+local windowWidth, windowHeight
+local xoff, yoff
+local gameAspect, windowAspect
 local scale
-local a_                             -- optimization value
+local a_
 
 local module =
 {
-  setColor = function(r1, g1, b1, a1) r, g, b, a = r1, g1, b1, a1 end, -- color of black bars
+  setColor = function(r1, g1, b1, a1) r, g, b, a = r1, g1, b1, a1 end,
   getColor = function() return r, g, b end,
 
-  setGame = function(w, h) gameWidth, gameHeight = w, h end, -- game width, height, scale
+  setGame = function(w, h) gameWidth, gameHeight = w, h end,
   getGame = function() return gameWidth, gameHeight end,
   getGameWidth = function() return gameWidth end,
   getGameHeight = function() return gameHeight end,
   getGameAspect = function() return gameAspect end,
 
-  getOff = function() return xoff, yoff end, -- offset of black bars
+  getOff = function() return xoff, yoff end,
   getX = function() return xoff end,
   getY = function() return yoff end,
 
   getScale = function() return scale end,
 
-  getWindow = function() return windowWidth, windowWidth end, -- window width, height, aspect
+  getWindow = function() return windowWidth, windowWidth end,
   getWindowWidth = function() return windowWidth end,
   getWindowHeight = function() return windowHeight end,
   getWindowAspect = function() return windowAspect end,
-
-  getBackground = function() return background end,
-  setBackground = function(back) -- change "background", if value ~= bool, then just switch "background" with not
-    if type(back) == "boolean" then
-      background = back
-    else
-      background = not background
-    end
-  end,
-
-  getBar1 = function() return x1, w1, y1, h1 end, -- if horizontal 1st = left, 2nd = right; if vertical 1st = upper, 2nd = bottom
+  
+  getBar1 = function() return x1, w1, y1, h1 end,
   getBar2 = function() return x2, w2, y2, h2 end,
 
   update = function()
@@ -82,8 +70,7 @@ local module =
   stop = function ()
     lovePop()
     lovePush("all")
-    if not background then loveColor(r, g, b, a)
-    else loveColor(loveBackground()) end
+    loveColor(r, g, b, a)
     loveRectangle("fill", x1, y1, w1, h1)
     loveRectangle("fill", x2, y2, w2, h2)
     lovePop()

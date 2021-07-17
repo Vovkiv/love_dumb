c8522a06fcff35827aef078566240a39acba3bfb
Vovkiv <54743395+Vovkiv@users.noreply.github.com>
2021-07-17 03:03:51 +0300

    Update resize_rectangle_with_mouse.lua

--- images_manipulations/resize_rectangle_with_mouse.lua
+++ images_manipulations/resize_rectangle_with_mouse.lua
@@ -1,24 +1,30 @@
---Resizing
---quite bad, but as example it's okay, i guess
 local obj = {x = 100, y = 100, w = 100, h = 100}
 local grab = false
+local x1, y1
 
-love.update = function()
-  if grab and not love.mouse.isDown(1) then
-    grab = false
-  end
+love.mousemoved = function(x, y)
   if grab then
-    local mx, my = love.mouse.getPosition()
-    obj.w = mx - obj.x
-    obj.h = my - obj.y
+    obj.w = x - obj.x
+    obj.h = y - obj.y
   end
 end
 
-love.mousepressed = function(x, y)
-  if x > obj.x and x < obj.x + obj.w and y > obj.y and y < obj.y + obj.h then
-    grab = true
-    end
+love.mousereleased = function(_, _, button)
+  if button == 1 then
+  grab = false
+  end
 end
+
+love.mousepressed = function(x, y, button)
+  if button == 1 then
+    if x > obj.x and x < obj.x + obj.w and y > obj.y and y < obj.y + obj.h then
+    grab = true
+    x1 = math.abs(obj.x - x)
+    y1 = math.abs(obj.y - y)
+    end
+  end
+end
+
 love.draw = function()
   love.graphics.rectangle("line", obj.x, obj.y, obj.w, obj.h)
 end

7dd0d50ff5996c69778159386bcf96923ecce97d
Vovkiv <54743395+Vovkiv@users.noreply.github.com>
2021-08-01 23:15:47 +0300

    Update aspect.lua

--- aspect/aspect.lua
+++ aspect/aspect.lua
@@ -1,74 +1,119 @@
--- https://github.com/Vovkiv/love_dumb/blob/main/aspect/aspect.lua
+--[[
+local aspect = require("aspect")
 
-local gameWidth, gameHeight = 800, 600
-local r, g, b, a = 0, 0, 0, 1
+aspect.setGame(1366, 768)
 
-local lovePop, lovePush, loveTranslate, loveScale, loveColor, loveRectangle, loveWidth, loveHeight, abs = love.graphics.pop, love.graphics.push, love.graphics.translate, love.graphics.scale, love.graphics.setColor, love.graphics.rectangle, love.graphics.getWidth, love.graphics.getHeight, math.abs -- don't do that shit
+love.update = function(dt)
+  aspect.update()
+end
 
-local x1, y1, w1, h1, x2, y2, w2, h2
-local windowWidth, windowHeight
-local xoff, yoff
-local gameAspect, windowAspect
-local scale
-local a_
+love.draw = function()
+  aspect.start()
+  
+  aspect.stop()
+end
 
-local module =
+]]
+
+local aspect =
 {
-  setColor = function(r1, g1, b1, a1) r, g, b, a = r1, g1, b1, a1 end,
-  getColor = function() return r, g, b, a end,
-
-  setGame = function(w, h) gameWidth, gameHeight = w, h end,
-  getGame = function() return gameWidth, gameHeight end,
-  getGameWidth = function() return gameWidth end,
-  getGameHeight = function() return gameHeight end,
-
-  getOff = function() return xoff, yoff end,
-  getXOff = function() return xoff end,
-  getYOff = function() return yoff end,
-
-  getScale = function() return scale end,
-
-  getWindow = function() return windowWidth, windowWidth end,
-  getWindowWidth = function() return windowWidth end,
-  getWindowHeight = function() return windowHeight end,
-
-  update = function()
-    windowWidth, windowHeight = loveWidth(), loveHeight()
-    gameAspect = gameWidth / gameHeight
-    windowAspect = windowWidth / windowHeight
-    if gameAspect > windowAspect then
-      scale = windowWidth / gameWidth
-      a_ = abs((gameHeight * scale - windowHeight) / 2)
-      x1, y1, w1, h1 = 0, 0, windowWidth, a_
-      x2, y2, w2, h2 = 0, windowHeight, windowWidth, -a_
-      xoff, yoff = 0, windowHeight / 2 - (scale * gameHeight) / 2
-    elseif gameAspect < windowAspect then
-      scale = windowHeight / gameHeight
-      a_ = abs((gameWidth * scale - windowWidth) / 2)
-      x1, y1, w1, h1 = 0, 0, a_, windowHeight
-      x2, y2, w2, h2 = windowWidth, 0, -a_, windowHeight
-      xoff, yoff = windowWidth / 2 - (scale * gameWidth) / 2, 0
-    else
-      scale = windowWidth / gameWidth
-      x1, y1, w1, h1 = 0, 0, 0, 0
-      x2, y2, w2, h2 = 0, 0, 0, 0
-      xoff, yoff = 0, 0
-    end
-  end,
-
-  start = function ()
-    lovePush()
-    loveTranslate(xoff, yoff)
-    loveScale(scale, scale)
-  end,
-  stop = function ()
-    lovePop()
-    lovePush("all")
-    loveColor(r, g, b, a)
-    loveRectangle("fill", x1, y1, w1, h1)
-    loveRectangle("fill", x2, y2, w2, h2)
-    lovePop()
-  end
+  _VERSION     = "1000",
+  _URL         = "https://github.com/Vovkiv/love_dumb/tree/main/aspect",
+  _DESCRIPTION = "Can handle best fit scaling for games with specific resolution",
+  _LICENSE     = "None",
 }
 
-return module
+aspect.scale = 0
+
+aspect.gameWidth = 800 -- size, to which game should be scaled to
+aspect.gameHeight = 600
+
+aspect.windowWidth = 0 -- size of window, which equal to love's love.graphics.getWidth\Height
+aspect.windowHeight = 0
+
+aspect.gameAspect = 0
+aspect.windowAspect = 0
+
+aspect.xOff = 0 -- offset of black bars
+aspect.yOff = 0
+
+aspect.x1, aspect.y1, aspect.w1, aspect.h1 = 0, 0, 0, 0 -- data of black bars
+aspect.x2, aspect.y2, aspect.w2, aspect.h2 = 0, 0, 0, 0
+
+aspect.r, aspect.g, aspect.b, aspect.a = 0, 0, 0, 0 -- colors of black bars
+
+aspect.setColor = function(r, g, b, a)
+  aspect.r = r
+  aspect.g = g
+  aspect.b = b
+  aspect.a = a
+end
+
+aspect.setGame = function(w, h)
+  aspect.gameWidth = w
+  aspect.gameHeight = h
+end
+
+aspect.update = function()
+  local x1, y1, w1, h1, x2, y2, w2, h2
+  local scale
+  local xOff, yOff
+  local a_ -- used to decrease calculations
+
+  local gameWidth, gameHeight = aspect.gameWidth, aspect.gameHeight
+  local windowWidth, windowHeight = love.graphics.getWidth(), love.graphics.getHeight()
+
+  local gameAspect = gameWidth / gameHeight
+  local windowAspect = windowWidth / windowHeight
+
+  if gameAspect > windowAspect then -- if window height > game height
+    scale = windowWidth / gameWidth
+    a_ = math.abs((gameHeight * scale - windowHeight) / 2)
+    x1, y1, w1, h1 = 0, 0, windowWidth, a_
+    x2, y2, w2, h2 = 0, windowHeight, windowWidth, -a_
+    xOff, yOff = 0, windowHeight / 2 - (scale * gameHeight) / 2
+
+  elseif gameAspect < windowAspect then -- if window width > game width
+    scale = windowHeight / gameHeight
+    a_ = math.abs((gameWidth * scale - windowWidth) / 2)
+    x1, y1, w1, h1 = 0, 0, a_, windowHeight
+    x2, y2, w2, h2 = windowWidth, 0, -a_, windowHeight
+    xOff, yOff = windowWidth / 2 - (scale * gameWidth) / 2, 0
+
+  else -- if window and game size equal
+    scale = windowWidth / gameWidth
+    x1, y1, w1, h1 = 0, 0, 0, 0
+    x2, y2, w2, h2 = 0, 0, 0, 0
+    xOff, yOff = 0, 0
+  end
+
+  aspect.x1, aspect.y1, aspect.w1, aspect.h1 = x1, y1, w1, h1
+  aspect.x2, aspect.y2, aspect.w2, aspect.h2 = x2, y2, w2, h2
+  aspect.xOff, aspect.yOff = xOff, yOff
+  aspect.scale = scale
+  aspect.windowWidth, aspect.windowHeight = windowWidth, windowHeight
+  aspect.gameAspect, aspect.windowAspect = gameAspect, windowAspect
+end
+
+aspect.start = function()
+  love.graphics.push()
+  love.graphics.translate(aspect.xOff, aspect.yOff) -- create offset of graphics
+  local scale = aspect.scale
+  love.graphics.scale(scale, scale)
+end
+
+aspect.stop = function()
+  love.graphics.pop()
+
+  love.graphics.push("all") -- "all" used here to return original colors after stop function
+  local r, g, b, a = aspect.a, aspect.g, aspect.b, aspect.a
+  local x1, y1, w1, h1 = aspect.x1, aspect.y1, aspect.w1, aspect.h1
+  local x2, y2, w2, h2 = aspect.x2, aspect.y2, aspect.w2, aspect.h2
+  
+  love.graphics.setColor(r, g, b, a)
+  love.graphics.rectangle("fill", x1, y1, w1, h1)
+  love.graphics.rectangle("fill", x2, y2, w2, h2)
+  love.graphics.pop()
+end
+
+return aspect
